91a6d36ea95e1d5d426fe309be6aae7cb882230f
Rassssven <dtimonea@gmail.com>
2023-12-21 21:11:12 +0100

    contact page

---
+++ event-ticket-app/resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+</head>
+<body>
+
+    <h1> Contact page </h1>
+
+    <div class="contact-box">
+        <h2> Contact information </h2>
+        <span> Email: RRZ@gmail.com </span> <br>
+        <span> Adress: Str. Teodor Mihali, Cluj-Napoca, Romania </span> <br>
+        <span> Telephone: 0751622632 </span> <br>
+        <span> Aditional information: You can contact us by sending an email to the adress, or by calling </span> <br>
+        <br>
+
+</body>
+</html>
+@endsection
