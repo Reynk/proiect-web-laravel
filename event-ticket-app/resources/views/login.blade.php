d5625435b20bbb7e02bbdc4f09a42c09067dcc22
Danny Zaporojanu <danny.zaporojanu@gmail.com>
2023-12-21 22:22:45 +0200

    3

--- event-ticket-app/resources/views/login.blade.php
+++ event-ticket-app/resources/views/login.blade.php
@@ -107,28 +107,4 @@
         </div>
     </div>
 </div>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-    }
-
-    h1 {
-        text-align: center;
-        color: #333;
-        margin-top: 50px;
-    }
-
-    .logout-button {
-        display: block;
-        width: 200px;
-        height: 50px;
-        margin: 0 auto;
-        background-color: #f44336;
-        color: white;
-        text-align: center;
-        line-height: 50px;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-</style>
 @endsection
--- event-ticket-app/resources/views/mainPage.blade.php
+++ event-ticket-app/resources/views/mainPage.blade.php
@@ -29,12 +29,19 @@
         border-radius: 5px;
         cursor: pointer;
     }
+    .info-text {
+            color: black;
+            font-size: 18px;
+            padding: 10px;
+            font-family: Arial, sans-serif;}
+            
 </style>
 </head>
 <body>
 
                 <div>
                     <h1>Home Page</h1>
+                    <h4 class="info-text">Immerse yourself in a world of cutting-edge technology, innovation, and knowledge-sharing. Explore a curated selection of IT conferences, workshops, and meetups tailored for tech enthusiasts, developers, and industry professionals. With seamless ticket booking and a community-centric approach, we connect you to the latest trends, expert insights, and networking opportunities. Elevate your IT experience with exclusive access to groundbreaking events and become part of a dynamic community that celebrates the forefront of technology. Your journey into the future of IT starts here—explore, connect, and stay ahead with RRZ tickets.</h4>
                 </div>
 </body>
 </html>
